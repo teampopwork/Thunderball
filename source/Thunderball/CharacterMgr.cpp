21f08ed71889e31421c97f27498d8c64abe09ec8
bananapizzuh <bananapizzuh@gmail.com>
2026-04-30 09:41:32 -0400

    add CharacterMgr function annotations

--- source/Thunderball/CharacterMgr.cpp
+++ source/Thunderball/CharacterMgr.cpp
@@ -2,118 +2,179 @@
 
 using namespace Sexy;
 
+// STUB: POPCAPGAME1 0x00473c30
 CharacterMgr::CharacterMgr()
 {
 }
+
+// SYNTHETIC: POPCAPGAME1 0x004746d0
+// Sexy::CharacterMgr::`scalar deleting destructor'
+
+// STUB: POPCAPGAME1 0x00473d10
 CharacterMgr::~CharacterMgr()
 {
 }
 
+// STUB: POPCAPGAME1 0x00438a10
 void CharacterMgr::CalcEyePos(bool param_1)
 {
 }
+
+// STUB: POPCAPGAME1 0x004410c0
 void CharacterMgr::CalcLookPos(Board* theBoard, bool param_2)
 {
 }
+
+// STUB: POPCAPGAME1 0x00438cf0
 void CharacterMgr::CalcPowerupActive(Board* theBoard)
 {
 }
+
+// STUB: POPCAPGAME1 0x004388a0
 void CharacterMgr::CheckCrossEyeFix()
 {
 }
+
+// STUB: POPCAPGAME1 0x00473d90
 void CharacterMgr::Clear(bool param_1)
 {
 }
+
+// STUB: POPCAPGAME1 0x00432920
 void CharacterMgr::CreateShooterImage(int param_1)
 {
 }
+
+// FUNCTION: POPCAPGAME1 0x0054b790
 void CharacterMgr::DoCharMove(int param_1, int param_2, bool param_3)
 {
 }
+
+// STUB: POPCAPGAME1 0x00432d70
 void CharacterMgr::DoPowerupBlink(int param_1)
 {
 }
+
+// STUB: POPCAPGAME1 0x00438cf0
 void CharacterMgr::DoPowerupCountBlink()
 {
 }
+
+// STUB: POPCAPGAME1 0x00439350
 void CharacterMgr::Draw(Graphics* g, Board* theBoard)
 {
 }
+
+// STUB: POPCAPGAME1 0x00438e30
 void CharacterMgr::DrawBack(Graphics* g1, Graphics* g2, Board* theBoard)
 {
 }
+
+// STUB: POPCAPGAME1 0x00441230
 void CharacterMgr::DrawFront(Graphics* g1, Graphics* g2, Board* theBoard)
 {
 }
+
+// STUB: POPCAPGAME1 0x00442c90
 std::string* CharacterMgr::GetCharacterDesc(int param_1)
 {
 	return NULL;
 }
 
+// STUB: POPCAPGAME1 0x0043e3b0
 int CharacterMgr::GetCharacterIdByName(std::string* param_1)
 {
 	return 0;
 }
 
+// STUB: POPCAPGAME1 0x004386e0
 int CharacterMgr::GetCharacterInfo(int param_1)
 {
 	return 0;
 }
 
+// STUB: POPCAPGAME1 0x00438760
 int CharacterMgr::GetCharacterInfoSafe(int param_1)
 {
 	return 0;
 }
 
+// STUB: POPCAPGAME1 0x00442c30
 int CharacterMgr::GetCharacterName(int param_1)
 {
 	return 0;
 }
 
+// STUB: POPCAPGAME1 0x00442cf0
 int CharacterMgr::GetCharacterTip(int param_1, unsigned long param_2)
 {
 	return 0;
 }
 
+// STUB: POPCAPGAME1 0x00442bd0
 int CharacterMgr::GetCurCharFrame(CharacterInfo* param_1)
 {
 	return 0;
 }
+
+// STUB: POPCAPGAME1 0x004747b0
 void CharacterMgr::InitDefault()
 {
 }
+
+// STUB: POPCAPGAME1 0x00474cb0
 void CharacterMgr::Load()
 {
 }
+
+// STUB: POPCAPGAME1 0x00474870
 void CharacterMgr::ReadConfig(std::string* param_1)
 {
 }
+
+// STUB: POPCAPGAME1 0x004657c0
 void CharacterMgr::ReaderCharacterConfig(ConfigParser* param_1, CharacterInfo* param_2)
 {
 }
+
+// STUB: POPCAPGAME1 0x00446490
 int CharacterMgr::ReadPowerupType(ConfigParser* param_1, std::string* param_2)
 {
 	return 0;
 }
+
+// STUB: POPCAPGAME1 0x000043e460
 void CharacterMgr::Resync()
 {
 }
+
+// STUB: POPCAPGAME1 0x0043e320
 int CharacterMgr::SetCurCharacter(int param_1)
 {
     return 0;
 }
+
+// STUB: POPCAPGAME1 0x00432b30
 void CharacterMgr::SetYinYangEye(bool param_1)
 {
 }
+
+// STUB: POPCAPGAME1 0x0043ff10
 void CharacterMgr::SyncState(DataSync& theState)
 {
 }
+
+// STUB: POPCAPGAME1 0x00439830
 void CharacterMgr::Update()
 {
 }
+
+// STUB: POPCAPGAME1 0x00439990
 void CharacterMgr::UpdatePaused()
 {
 }
+
+// STUB: POPCAPGAME1 0x00439a60
 void CharacterMgr::UpdateSlowMo()
 {
 }
