e84514e3314126b5b86650cda648d66a83c09715
bananapizzuh <bananapizzuh@gmail.com>
2026-05-19 14:42:15 -0400

    add constructor to EndLevelDialog

--- source/Thunderball/EndLevelDialog.cpp
+++ source/Thunderball/EndLevelDialog.cpp
@@ -3,7 +3,7 @@
 using namespace Sexy;
 
 // STUB: POPCAPGAME1 0x004a1a10
-EndLevelDialog::EndLevelDialog(Board* param_1)
+EndLevelDialog::EndLevelDialog(Board* param_1) : ThunderDialog(0, false, "", "", "", 0)
 {
 }
 
