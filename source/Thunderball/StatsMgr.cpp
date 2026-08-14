8b7bb297c4e58930b95385498f8a8498ebe34675
bananapizzuh <jay@bananapizzuh.dev>
2026-08-13 22:47:00 -0400

    add addresses for StatsMgr functions

--- source/Thunderball/StatsMgr.cpp
+++ source/Thunderball/StatsMgr.cpp
@@ -2,63 +2,77 @@
 
 using namespace Sexy;
 
+// STUB: POPCAPGAME1 0x00461b00
 StatsMgr::StatsMgr()
 {
 }
 
+// STUB: POPCAPGAME1 0x004623e0
 LevelStats* StatsMgr::GetStageStatsForLevel(std::string& param_1)
 {
     return NULL;
 }
 
+// STUB: POPCAPGAME1 0x004623d0
 LevelStats* StatsMgr::GetStatsForLevel(std::string& param_1)
 {
     return NULL;
 }
 
+// STUB: POPCAPGAME1 0x00459340
 TrophyStats* StatsMgr::GetStatsForTrophy(int param_1)
 {
     return NULL;
 }
 
+// STUB: POPCAPGAME1 0x00437780
 void StatsMgr::IncCharacterCount(int param_1)
 {
 }
 
+// STUB: POPCAPGAME1 0x004376f0
 void StatsMgr::IncCharacterLossCount(int param_1)
 {
 }
 
+// STUB: POPCAPGAME1 0x004377c0
 void StatsMgr::IncCharacterWinCount(int param_1)
 {
 }
 
+// STUB: POPCAPGAME1 0x00437820
 void StatsMgr::IncStageCharacterLossCount(int param_1)
 {
 }
 
+// STUB: POPCAPGAME1 0x00437800
 void StatsMgr::IncStageCharacterWinCount(int param_1)
 {
 }
 
+// STUB: POPCAPGAME1 0x004377a0
 void StatsMgr::IncTrophyCharacterCount(int param_1)
 {
 }
 
+// STUB: POPCAPGAME1 0x00452850
 void StatsMgr::Clear()
 {
 }
 
+// STUB: POPCAPGAME1 0x00461d20
 bool StatsMgr::Load(char* path)
 {
     return false;
 }
 
+// STUB: POPCAPGAME1 0x004620a0
 bool StatsMgr::Save(char* path)
 {
     return false;
 }
 
+// STUB: POPCAPGAME1 0x00461bd0
 void StatsMgr::SyncState(DataSync& sync)
 {
 }
